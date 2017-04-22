3af69e3d6b74590d873afbe120e69fba2c3e530f
bburling1 <b.burling1996@gmail.com>
2017-04-22 23:49:09 +1000

    thread add form

---
+++ view/thread_add_form.php
@@ -0,0 +1,62 @@
+<?php
+  //start session management
+  session_start();
+  //connect to the database
+  require('../model/database.php');
+  //retrieve functions
+  require('../model/functions_categories.php');
+  require('../model/functions_threads.php');
+  require('../model/functions_messages.php');
+
+  $title = "Create a Thread";
+
+  include "header.php"
+?>
+
+<section class="section">
+  <div class="container content">
+    <form class="form" action="../controller/thread_add_process.php" method="post">
+      <h2 class="form-title">Create a New Thread</h2>
+      <?php
+        //user messages
+        $message = user_message();
+      ?>
+      <div class="field">
+        <label class="label"><b>Subject</b></label>
+        <p class="control">
+          <input class="input" type="text" placeholder="Thread Title/Subject" name="cat_name" required>
+        </p>
+      </div>
+      <div class="field">
+        <label class="label"><b>Thread Content</b></label>
+        <p class="control">
+          <textarea class="textarea" placeholder="Thread Content" name="cat_description"></textarea>
+        </p>
+      </div>
+      <div class="field">
+        <label class="label"><b>Category</b></label>
+        <p class="control">
+          <span class="select">
+            <select name="cat_id">
+            <?php
+              $result = get_categories();
+              foreach ($result as $row):
+            ?>
+              <option value="<?php echo $row['cat_id'];?>"><?php echo $row['cat_name']; ?></option>
+            <?php endforeach; ?>
+            </select>
+          </span>
+        </p>
+      </div>
+      <div class="field">
+        <p class="control">
+          <button class="button is-primary" type="submit">Submit</button>
+        </p>
+      </div>
+    </form>
+  </div>
+</section>
+
+<?php
+require('footer.php');
+?>
