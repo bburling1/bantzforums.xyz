ebf17e0a1ccc033df7ccaf96711d74c5d42b0aa3
bburling1 <b.burling1996@gmail.com>
2017-04-28 23:09:44 +1000

    updates

--- model/functions_permissions.php
+++ model/functions_permissions.php
@@ -4,20 +4,20 @@
       header('location:../index.php');
       exit();
     } elseif($_SESSION['permissions'] != 'admin') {
-      //redirect to homepage if user isn't an admin
-      header("location:../index.php");
       //set error message
       $_SESSION['error'] = "You do not have the required permissions to access this page";
+      //redirect to homepage if user isn't an admin
+      header("location:../index.php");
       exit();
     }
   }
 
   function is_user_logged_in(){
     if(!isset($_SESSION['user'])){
-      //redirect to homepage is user isn't logged in
-      header("location:../view/categories.php");
       //set error message
       $_SESSION['error'] = "Please log in to access this page";
+      //redirect to homepage is user isn't logged in
+      header("location:../view/categories.php");
     }
   }
  ?>
