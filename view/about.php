3743557aa4703e640903e62a3bc1dcaa8ba13d7f
bburling1 <b.burling1996@gmail.com>
2017-05-19 11:41:29 +1000

    update

--- view/about.php
+++ view/about.php
@@ -4,13 +4,13 @@ include "header.php"
 
 <div class="container content">
   <section class="section">
+    <pre>
     <?php
-    //user messages
-    $message = user_message();
+    $json = file_get_contents('https://oc1.api.riotgames.com/lol/static-data/v3/champions?dataById=true&api_key=RGAPI-12409ba4-3bb9-44d8-abd3-46263eecc0f1');
+    $data = json_decode($json, TRUE);
+    print_r($data);
     ?>
-    <h1 class="title">Welcome to League of Forums!</h1>
-    <h2 class="subtitle is-4">About Us</h2>
-    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque sit amet vulputate tortor. Morbi volutpat est purus, eu blandit augue placerat non. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vivamus nec sem et arcu rutrum tempus et at nisi. Vivamus laoreet, sem feugiat pellentesque consequat, erat libero bibendum mi, vitae ullamcorper odio elit a odio. Vivamus volutpat augue justo, ut ornare nisi elementum in. Etiam sollicitudin mi a lobortis tincidunt. Praesent dictum purus id iaculis pharetra. Nullam luctus ultrices porttitor. Cras eget tellus sapien. Donec volutpat mauris eu lorem gravida finibus. Cras mollis sit amet eros eu viverra. Duis turpis libero, feugiat sed nibh quis, eleifend varius tellus. Aenean condimentum, turpis at aliquam ultrices, est purus luctus nisl, non interdum enim tortor in sem.</p>
+    </pre>
   </section>
 </div>
 
